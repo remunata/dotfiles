59b71a2c3b4407287e255e2cff8ca441ade6bebc
Mahendra Dinata <remdinata@gmail.com>
2025-01-31 01:57:59 +0700

    remove gtk3 prefer dark theme rule

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -34,11 +34,6 @@
       size = 11;
     };
 
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
     gtk4.extraConfig = {
       Settings = ''
         gtk-application-prefer-dark-theme=1
