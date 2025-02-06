debfad04cd1b79557c4e8f6e5c59473be4dbc627
Mahendra Dinata <remdinata@gmail.com>
2025-02-06 13:15:27 +0700

    add support for ntfs drive format

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -40,6 +40,9 @@
     };
   };
 
+  # Support NTFS file system for windows partition.
+  boot.supportedFilesystems = [ "ntfs" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
