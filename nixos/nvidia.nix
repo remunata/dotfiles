e4568dc4118ea18932f4e3e70c0d45b42489991e
Mahendra Dinata <remdinata@gmail.com>
2025-04-07 11:34:49 +0700

    add option nvidia.open

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -33,6 +33,7 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.production;
+    open = false;
 
     prime = {
       sync.enable = true;
