0572176c34e7d5ce98b864302002842fc3aecbdb
Mahendra Dinata <remdinata@gmail.com>
2025-04-07 12:40:37 +0700

    change window border color

--- home-manager/apps/hyprland/settings.nix
+++ home-manager/apps/hyprland/settings.nix
@@ -24,8 +24,8 @@
       gaps_out = 5;
       border_size = 2;
 
-      "col.active_border" = "rgba(ca9ee6ff) rgba(f2d5cfff) 45deg";
-      "col.inactive_border" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
+      "col.active_border" = "rgba(cba6f7ff) rgba(89b4faff) rgba(94e2d5ff) 10deg";
+      "col.inactive_border" = "0xff313244";
     };
 
     misc = {
