db92592632ec0716bfa640b3f333c57d892aa49a
Mahendra Dinata <remdinata@gmail.com>
2025-09-28 17:09:04 +0700

    refactor sddm config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,7 +7,7 @@
     ./nvidia.nix
     ./packages.nix
     ./system.nix
-    ./sddm-avatar.nix
+    ./sddm.nix
     ./user.nix
     ./hardware-configuration.nix
   ];
--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,26 +1,4 @@
 { inputs, pkgs, ... }:
-
-let
-  sddm-bg = pkgs.fetchurl {
-    url = "https://www.desktophut.com/files/oA5LA5QU3UCjLhA_arlecchino-the-knave-genshin-impact.3840x2160.mp4";
-    hash = "sha256-tYB8vlvG7qdcWCTfRV7rnhzTSngJElUieN8IOsYweBY=";
-  };
-  sddm-theme = inputs.silentSDDM.packages.${pkgs.system}.default.override {
-    theme = "rei";
-    extraBackgrounds = [ sddm-bg ];
-    theme-overrides = {
-      "LoginScreen.LoginArea.Avatar" = {
-        shape = "circle";
-      };
-      LockScreen = {
-        background = "${sddm-bg.name}";
-      };
-      General = {
-        "animated-background-placeholder" = "";
-      };
-    };
-  };
-in
 {
   # Hyprland.
   programs.hyprland = {
@@ -30,26 +8,8 @@ in
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
-  # SDDM login manager.
-  services.displayManager.sddm = {
-    enable = true;
-    package = pkgs.kdePackages.sddm;
-    wayland.enable = true;
-    theme = sddm-theme.pname;
-    extraPackages = sddm-theme.propagatedBuildInputs;
-    settings = {
-      # required for styling the virtual keyboard
-      General = {
-        GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
-        InputMethod = "qtvirtualkeyboard";
-      };
-    };
-  };
-
   # Dependencies.
   environment.systemPackages = with pkgs; [
-    sddm-theme
-    sddm-theme.test
     lxqt.lxqt-policykit
     libsForQt5.qt5.qtgraphicaleffects
     inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
