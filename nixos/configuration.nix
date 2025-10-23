fe7c9dddc5a2bef4c6c05341abb3f93dff87a835
Mahendra Dinata <remdinata@gmail.com>
2025-10-23 20:47:11 +0700

    disable auto garbage collection cause its pain

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -49,13 +49,6 @@
   # Support NTFS file system for windows partition.
   boot.supportedFilesystems = [ "ntfs" ];
 
-  # Automatic system specific garbage collection every 30d.
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
