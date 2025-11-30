02836a66be9ea59ecafd68cdbd4ed85164627400
Mahendra Dinata <remdinata@gmail.com>
2025-11-30 13:55:33 +0700

    add wezterm event listener to remove padding when entering neovim

--- home-manager/apps/wezterm.nix
+++ home-manager/apps/wezterm.nix
@@ -18,6 +18,21 @@
         bottom = 15,
       }
 
+      -- Event listener to dynamicall change padding
+      wezterm.on('user-var-changed', function(window, pane, name, value)
+        if name == "is_nvim" then
+          local overrides = window:get_config_overrides() or {}
+          if value == "0" then
+            -- Set padding to 0 when entering Neovim
+            overrides.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+          else
+            -- Restore default padding when leaving
+            overrides.window_padding = nil
+          end
+          window:set_config_overrides(overrides)
+        end
+      end)
+
       -- Helper functions
       -- Check if file exists
       local function file_exists(path)
