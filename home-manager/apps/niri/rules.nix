04155c45507cb8841fd9ed81aa173e76fb1bce50
Mahendra Dinata <remdinata@gmail.com>
2026-01-16 22:43:42 +0700

    update window rules

--- home-manager/apps/niri/rules.nix
+++ home-manager/apps/niri/rules.nix
@@ -9,34 +9,27 @@
         top-right = 12.;
       };
       clip-to-geometry = true;
-    }
-    {
-      matches = [ { app-id = "r#\"^org\.gnome\.\"#"; } ];
-      draw-border-with-background = false;
-      geometry-corner-radius = {
-        bottom-left = 12.;
-        bottom-right = 12.;
-        top-left = 12.;
-        top-right = 12.;
-      };
-      clip-to-geometry = true;
+      open-maximized = true;
     }
     {
       matches = [ { app-id = "wezterm"; } ];
       opacity = 0.8;
       draw-border-with-background = false;
-      open-maximized = true;
     }
     {
-      matches = [ { app-id = "quickshell"; } ];
+      matches = [
+        { app-id = "quickshell"; }
+        { app-id = "pavucontrol"; }
+      ];
       open-floating = true;
     }
     {
       matches = [
-        { app-id = "vivaldi"; }
-        { app-id = "libreoffice"; }
+        { app-id = "thunar"; }
+        { app-id = "qimgv"; }
+        { app-id = "org.gnome.Evince"; }
       ];
-      open-maximized = true;
+      open-maximized = false;
     }
   ];
 }
